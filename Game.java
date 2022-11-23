fb1889fb1b7ce1261a4bc83f281d7f1de3c4ecce
Sickan666 <jesabo2001@skola.goteborg.se>
2022-11-23 17:56:32 +0100

    Revert "Revert "more code in Game.java""
    This reverts commit 4b8cda39e6933e3f9279bf41c7a295b866578311.

--- Game.java
+++ Game.java
@@ -1,18 +1,40 @@
-
 import java.io.Console;
-public class Game {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
-        System.out.println("Hello Java");
-	System.out.println("Welcome to the Countries game.");
+public class Game {
+
+    public static void main(String[] args) {
+	System.out.println("Countries Game");
 	Console console = System.console();
-	String answer = console.readLine("Which continent does Sweden belong to? ");
-	String continent= "Europe";
-	if (answer.equals(continent)) {
+	BufferedReader br = null;
+	
+	try {
+	    br = new BufferedReader(new FileReader("continents.csv"));
+	    String line;
+	
+	    while((line=br.readLine()) != null) {
+		String[] cols = line.split(",");
+		String country = cols[0];
+		String continent= cols[1];
+		String answer= console.readLine("which continent does " + country+ " belong to?");
+	
+	if(answer.equals(continent)) {
 	    System.out.println("Correct!");
+
 	}
-	 else {	
-	   System.out.println("No, the answer is " + continent);
-	}	
+	else {
+	    System.out.println("No, the answer is " + continent);
+	}
     }
-}	
+
+	}catch(FileNotFoundException e) {
+	    e.printStackTrace();
+
+	}catch(IOException e) {
+	    e.printStackTrace();
+	}
+    }
+}
